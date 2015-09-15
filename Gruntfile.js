ac9b87f60a9dbe2828a3c666c9714b4be9272180
Coby Tamayo <me@cobytamayo.com>
2015-09-15 16:38:44 -0700

    Clean out default themes (except twentyfifteen) and plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,12 +8,21 @@ module.exports = function(grunt) {
         composerLocation: '/home/tamayo/bin/composer' 
       },
       wordpress: {}
-    }
+    },
+    clean: [
+      'public/wp/wp-content/plugins/hello.php',
+      'public/wp/wp-content/themes/twentyten',
+      'public/wp/wp-content/themes/twentyeleven',
+      'public/wp/wp-content/themes/twentytwelve',
+      'public/wp/wp-content/themes/twentythirteen',
+      'public/wp/wp-content/themes/twentyfourteen'
+    ]
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-composer');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task.
-  grunt.registerTask('default', ['composer:wordpress:install']);
+  grunt.registerTask('default', ['composer:wordpress:install', 'clean']);
 };
